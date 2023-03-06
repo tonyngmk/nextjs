172b1ceb7af7d3933e1bef30f3db7c964b07ec84
Tony <tonyngmengkiat@gmail.com>
2023-03-06 16:57:07 +0800

    add api and images

--- next13-experiment/app/page.jsx
+++ next13-experiment/app/page.jsx
@@ -1,13 +1,27 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from './page.module.css'
+import Movie from "./Movie"
 
 const inter = Inter({ subsets: ['latin'] })
 
-export default function Home() {
+export default async function Home() {
+  const data = await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${process.env.API_KEY}`)
+  const res = await data.json()
+  console.log(res)
   return (
     <main>
       <h1 className="text-lg">Hello Next 13 🔥</h1>
+      {res.results.map(movie =>(
+        // <h1>{movie.title}</h1> 
+        <Movie 
+          key={movie.id}
+          id={movie.id}
+          poster_path={movie.poster_path}
+          release_date={movie.release_date}
+        />
+      ))}
+
     </main>
   )
 }
--- next13-experiment/next.config.js
+++ next13-experiment/next.config.js
@@ -3,6 +3,13 @@ const nextConfig = {
   experimental: {
     appDir: true,
   },
+  images: {
+    domains: ["image.tmdb.org"]
+    // protocol: "https",
+    // hostname: "image.tmdb.org",
+    // port: '',
+    // pathname: ''
+  }
 }
 
 module.exports = nextConfig
